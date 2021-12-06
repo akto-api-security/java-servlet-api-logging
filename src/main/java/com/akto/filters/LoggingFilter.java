83705bf0c278e82a87c196253f7f771056a4dac8
Avneesh Hota <iamavneesh.hota@gmail.com>
2021-12-06 18:47:31 +0530

    migrated from object to string value

--- src/main/java/com/akto/filters/LoggingFilter.java
+++ src/main/java/com/akto/filters/LoggingFilter.java
@@ -18,11 +18,13 @@ public class LoggingFilter implements Filter {
     private String brokerIP;
     private String topic;
     private Kafka kafka;
+    private String akto_account_id;
 
     @Override
     public void init(FilterConfig filterConfig){
         brokerIP = filterConfig.getInitParameter("brokerIP");
         topic = filterConfig.getInitParameter("topic");
+        this.akto_account_id  = filterConfig.getInitParameter("akto_account_id");
         kafka = new Kafka(brokerIP);
     }
 
@@ -37,10 +39,16 @@ public class LoggingFilter implements Filter {
         chain.doFilter(aa,bb);
 
         if (httpServletRequest!=null) {
-            Map<String,Object> finalMap = new HashMap<>();
-            finalMap.put("time", (int)(System.currentTimeMillis() / 1000L));
+            Map<String,String> finalMap = new HashMap<>();
+            finalMap.put("time", (int)(System.currentTimeMillis() / 1000L)+"");
 
-            logRequest(aa, finalMap);
+            String contentType = bb.getContentType();
+            List<String> dd = Arrays.asList(contentType.split(";"));
+            if (contentType == null || !dd.contains("application/json")) {
+                return ;
+            }
+
+            logRequest(akto_account_id,aa, finalMap);
             logResponse(bb, finalMap);
             
             final String json = mapper.writeValueAsString(finalMap);
@@ -50,39 +58,41 @@ public class LoggingFilter implements Filter {
 
     }
 
-    private void logRequest(final CustomHttpServletRequestWrapper request, Map<String, Object> finalMap) throws IOException {
+    private void logRequest(String akto_account_id,final CustomHttpServletRequestWrapper request, Map<String, String> finalMap) throws IOException {
+        finalMap.put("akto_account_id", akto_account_id);
         finalMap.put("path", request.getRequestURI());
         finalMap.put("method", request.getMethod());
         finalMap.put("ip", request.getRemoteAddr());
         finalMap.put("type", request.getProtocol());
-        Map<String,Object> headers = new HashMap<>();
+        Map<String,String> headers = new HashMap<>();
         Enumeration<String> headerNames = request.getHeaderNames();
         while (headerNames.hasMoreElements()) {
             String headerName = headerNames.nextElement();
             headers.put(headerName, request.getHeader(headerName));
         }
-        finalMap.put("requestHeaders",headers);
+        String json = mapper.writeValueAsString(headers);
+        finalMap.put("requestHeaders", json);
         ServletInputStream inputStream = request.getInputStream();
         String req = IOUtils.toString(inputStream, "UTF-8");
         finalMap.put("requestPayload", req);
     }
 
-    private void logResponse(final CustomHttpServletResponseWrapper response, Map<String, Object> finalMap) throws IOException {
-        Map<String,Object> headers = new HashMap<>();
+    private void logResponse(final CustomHttpServletResponseWrapper response, Map<String, String> finalMap) throws IOException {
+        Map<String,String> headers = new HashMap<>();
         Collection<String> headerNames = response.getHeaderNames();
         for (String headerName: headerNames) {
             headers.put(headerName, response.getHeader(headerName));
         }
-        finalMap.put("responseHeaders", headers);
+
+        String json = mapper.writeValueAsString(headers);
+
+        finalMap.put("responseHeaders", json);
         String res = new String(response.toByteArray(), StandardCharsets.UTF_8);
-        finalMap.put("statusCode", response.getStatus());
+        finalMap.put("statusCode", response.getStatus() + "");
         finalMap.put("status", null);
         String contentType = response.getContentType();
         finalMap.put("contentType", contentType);
 
-        if (contentType == null || !contentType.split(";")[0].equals("application/json")) {
-            res = "{}";
-        }
         finalMap.put("responsePayload",res);
 
     }
